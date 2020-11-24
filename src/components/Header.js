ac18e8195580b16ce262803eae2f6706b05a1c3b
Tatiana Grigorieva <tsg1204@gmail.com>
2020-11-24 14:36:25 -0500

    init custom theme added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core';
 import { HeadsetTwoTone } from '@material-ui/icons';
 
+const useStyles = makeStyles(theme => ({
+    title: {
+        marginLeft: theme.spacing(2)
+    }
+}))
+
 function Header() {
+    const classes = useStyles()
+
     return (
-        <AppBar position='fixed'>
+        <AppBar position='fixed' color="primary">
             <Toolbar>
                 <HeadsetTwoTone />
-                <Typography variant="h6" component="h1">
+                <Typography className={classes.title} variant="h6" component="h1">
                     Apollo Music Share
                 </Typography>
             </Toolbar>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { CssBaseline, MuiThemeProvider } from '@material-ui/core';
+import theme from './theme';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+    </MuiThemeProvider>
+    , document.getElementById('root'));
 
