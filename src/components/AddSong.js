1e6f313b4a1cb8bf8d93b1bc0cf8a960bbe80282
Tatiana Grigorieva <tsg1204@gmail.com>
2020-11-24 15:02:07 -0500

    add song func added

--- src/components/AddSong.js
+++ src/components/AddSong.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { TextField, InputAdornment, Button } from '@material-ui/core';
+import { AddBoxOutlined, Link } from '@material-ui/icons';
 
 function AddSong() {
-    return <div>Add song</div>
+    return (
+        <div>
+            <TextField
+                placeholder="Add Youtube or Soundcloud Url"
+                fullWidth
+                margin="normal"
+                type="url"
+                InputProps={{
+                    startAdorment: (
+                        <InputAdornment position="start">
+                            <Link />
+                        </InputAdornment>
+                    )
+                }}
+            />
+            <Button
+                variant="contained"
+                color="primary"
+                endIcon={<AddBoxOutlined />}
+            >
+                Add
+            </Button>
+    
+        </div>
+    )
 }
 
 export default AddSong;
